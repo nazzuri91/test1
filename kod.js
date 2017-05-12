84a259c5c539a4c0b92d8ab2f08f00832087f4bb
nazzuri91 <nazzuri91@gmail.com>
2017-05-12 11:25:13 +0200

    nowe pliczki

---
+++ kod.js
@@ -0,0 +1,17 @@
+var btn = document.getElementById("btn");
+var backBtn = document.getElementById("backBtn");
+var body = document.body;
+var wrapper = document.querySelector(".wrapper");
+var modle = document.querySelector(".row3");
+btn.addEventListener("click", function(){
+	body.classList.add("setOpacity");
+	wrapper.classList.add("displayNone");
+	modle.classList.add("displayBlock");
+	modle.classList.add("clearOpacity");
+}, false);
+backBtn.addEventListener("click", function(){
+	body.classList.remove("setOpacity");
+	wrapper.classList.remove("displayNone");
+	modle.classList.remove("displayBlock");
+	modle.classList.remove("clearOpacity");
+}, false);
